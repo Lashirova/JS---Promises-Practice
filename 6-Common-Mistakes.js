fc07699a952b80d54051babe5e02ff2d9dda35b0
Lachin Ashirova <lachinazra@gmail.com>
2020-11-23 20:08:15 +0000

    Initial commit

---
+++ 6-Common-Mistakes.js
@@ -0,0 +1,16 @@
+const {checkInventory, processPayment, shipOrder} = require('./5.1-helper.js');
+
+const order = {
+  items: [['sunglasses', 1], ['bags', 2]],
+  giftcardBalance: 79.82
+};
+
+// Refactor, or rewrite, the code to avoid the two common mistakes: nesting and forgetting to return properly.
+
+checkInventory(order).then((resolvedValueArray) => {
+  return processPayment(resolvedValueArray);
+}).then((resolvedValueArray2) => {
+  return shipOrder(resolvedValueArray2);
+}).then((successMessage) => {
+  console.log(successMessage);
+});
---
+++ index.js
@@ -0,0 +1,9 @@
+
+// let orderPromise = require('./1-Promise-Object.js')
+// console.log(orderPromise)
+
+// let timeout = require('./2-setTimeout-Function.js')
+// require('./4-catch-Function.js')
+// require('./5-Chaining-Multiple-Promises.js')
+// require('./6-Common-Mistakes.js')
+require('./7-promiseAll-Funciton.js')
